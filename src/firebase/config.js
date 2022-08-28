f6ba11b975790761761e91c38613ce67b40daabe
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2022-08-28 16:43:13 -0300

    Criado arquivo de configuração do firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firebase'
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAV2WPrme6LjrnTEk07j1Xlx_QoSOmYsXk",
+    authDomain: "miniblog-curso-reactjs.firebaseapp.com",
+    projectId: "miniblog-curso-reactjs",
+    storageBucket: "miniblog-curso-reactjs.appspot.com",
+    messagingSenderId: "41939716023",
+    appId: "1:41939716023:web:24e6d7a8ee8af5f8564a32"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app)
+
+export { db }
