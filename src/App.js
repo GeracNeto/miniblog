474b420586312df00bac406d0722905200c78c48
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2022-08-28 16:55:59 -0300

    Criada as rotas das pages 'Home' e 'About'

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+// CSS
 import './App.css';
+
+// Libs
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// Pages
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
 
 function App() {
   return (
     <div className="App">
-      <h1>MiniBlog</h1>
+      <BrowserRouter>
+        <div className="container">
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
