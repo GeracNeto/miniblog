24b4ce0ba54c6c37c5d41832479a49a24ec29aa2
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2022-08-30 20:02:25 -0300

    Atualizado estilos do navbar e footer

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,14 +6,14 @@ import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
     return (
-        <nav>
-            <NavLink to='/'>Mini <span>Blog</span></NavLink>
-            <ul>
+        <nav className={styles.navbar}>
+            <NavLink to='/' className={styles.brand}>Mini <span>Blog</span></NavLink>
+            <ul className={styles.links_list}>
                 <li>
-                    <NavLink to='/'>Home</NavLink>
+                    <NavLink to='/' className={({ isActive }) => (isActive ? styles.active : '')}>Home</NavLink>
                 </li>
                 <li>
-                    <NavLink to='/about'>About</NavLink>
+                    <NavLink to='/about' className={({ isActive }) => (isActive ? styles.active : '')}>About</NavLink>
                 </li>
             </ul>
         </nav>
