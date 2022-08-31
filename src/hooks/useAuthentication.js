ada9dc129ab57fd812054d7403e96419c136eaa9
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2022-08-31 20:48:57 -0300

    Criado lógica de login do usuário

--- src/hooks/useAuthentication.js
+++ src/hooks/useAuthentication.js
@@ -65,7 +65,7 @@ export const useAuthentication = () => {
                 systemErrorMessage = "E-mail já cadastrado."
             }
             else {
-                systemErrorMessage = "Ocorreu um erro, por favor tente mais tarde..."
+                systemErrorMessage = "Ocorreu um erro, por favor tente mais tarde."
             }
 
             setLoading(false)
@@ -81,9 +81,43 @@ export const useAuthentication = () => {
         signOut(auth)
     }
 
+    // Login - sign in
+    const login = async (data) => {
+
+        checkIfIsCancelled()
+
+        setLoading(true)
+
+        setError(false)
+
+        try {
+
+            await signInWithEmailAndPassword(auth, data.email, data.password)
+
+            setLoading(false)
+
+        } catch (error) {
+
+            let systemErrorMessage
+
+            if (error.message.includes("user-not-found")) {
+                systemErrorMessage = "Usuário não encontrado."
+            }
+            else if (error.message.includes("wrong-password")) {
+                systemErrorMessage = "Senha incorreta."
+            }
+            else {
+                systemErrorMessage = "Ocorreu um erro, por favor tente mais tarde."
+            }
+            setError(systemErrorMessage)
+
+            setLoading(false)
+        }
+    }
+
     useEffect(() => {
         return () => setCancelled(true) // Memory leak
     })
 
-    return { auth, createUser, error, loading, logout }
+    return { auth, createUser, error, loading, logout, login }
 }
