b24c4206a01aeba2835ee9bf3a5ac7b7127be07a
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2022-09-01 16:25:36 -0300

    Atualizada página da home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,46 @@
 // CSS
 import styles from './Home.module.css'
 
+// Libs
+import { useNavigate, Link } from 'react-router-dom'
+
+// Hooks
+import { useState } from 'react'
+
+// Components
+
+
 const Home = () => {
+
+    const [query, setQuery] = useState('')
+    const [posts] = useState([])
+
+    const handleSubmit = (e) => {
+
+        e.preventdefault()
+    }
+
     return (
-        <div>Home</div>
+        <div className={styles.home}>
+            <h1>Veja os nossos posts mais recentes</h1>
+            <form onSubmit={handleSubmit} className={styles.search_form}>
+                <input
+                    type="text"
+                    placeholder='Ou busque por tags...'
+                    onChange={e => setQuery(e.target.value)}
+                />
+                <button className='btn btn-dark'>Pesquisar</button>
+            </form>
+            <div>
+                <h1>Posts...</h1>
+                {posts && posts.length === 0 && (
+                    <div className={styles.noposts}>
+                        <p>Não foram encontrados posts</p>
+                        <Link to='/posts/create' className='btn'>Criar primeiro post</Link>
+                    </div>
+                )}
+            </div>
+        </div>
     )
 }
 
