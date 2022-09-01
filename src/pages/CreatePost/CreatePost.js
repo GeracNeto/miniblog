c57cdf3278e3eebc6a53b4bc172432e6709ad1a8
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2022-09-01 16:07:08 -0300

    Ciado inserção de dados no firebase e validação dos dados

--- src/pages/CreatePost/CreatePost.js
+++ src/pages/CreatePost/CreatePost.js
@@ -1,24 +1,67 @@
-// CSS
-import styles from './CreatePost.module.css'
+import styles from "./CreatePost.module.css";
 
-// Hooks
-import { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
-
-// Context
-import { useAuthValue } from '../../context/AuthContext'
+import { useState } from "react";
+import { useInsertDocument } from "../../hooks/useInsertDocument";
+import { useNavigate } from "react-router-dom";
+import { useAuthValue } from "../../context/AuthContext";
 
 const CreatePost = () => {
+    const [title, setTitle] = useState("");
+    const [image, setImage] = useState("");
+    const [body, setBody] = useState("");
+    const [tags, setTags] = useState([]);
+    const [formError, setFormError] = useState("");
 
-    const [title, setTitle] = useState('')
-    const [image, setImage] = useState('')
-    const [body, setBody] = useState('')
-    const [tags, setTags] = useState([])
-    const [formError, setFormError] = useState('')
+    const { user } = useAuthValue();
+
+    const navigate = useNavigate();
+
+    const { insertDocument, response } = useInsertDocument("posts");
 
     const handleSubmit = (e) => {
-        e.preventdefault()
-    }
+        e.preventDefault();
+        setFormError("");
+
+        // validate image
+        try {
+            new URL(image);
+        } catch (error) {
+            setFormError("A imagem precisa ser uma URL.");
+        }
+
+        // create tags array
+        const tagsArray = tags.split(",").map((tag) => tag.trim().toLowerCase());
+
+        // check values
+        if (!title || !image || !tags || !body) {
+            setFormError("Por favor, preencha todos os campos!");
+        }
+
+        console.log(tagsArray);
+
+        console.log({
+            title,
+            image,
+            body,
+            tags: tagsArray,
+            uid: user.uid,
+            createdBy: user.displayName,
+        });
+
+        if (formError) return
+
+        insertDocument({
+            title,
+            image,
+            body,
+            tags: tagsArray,
+            uid: user.uid,
+            createdBy: user.displayName,
+        });
+
+        // redirect to home page
+        navigate("/");
+    };
 
     return (
         <div className={styles.create_post}>
@@ -29,10 +72,10 @@ const CreatePost = () => {
                     <span>Título:</span>
                     <input
                         type="text"
-                        name='title'
-                        placeholder='Pense em um bom título'
+                        name="text"
                         required
-                        onChange={e => setTitle(e.target.value)}
+                        placeholder="Pense num bom título..."
+                        onChange={(e) => setTitle(e.target.value)}
                         value={title}
                     />
                 </label>
@@ -40,20 +83,20 @@ const CreatePost = () => {
                     <span>URL da imagem:</span>
                     <input
                         type="text"
-                        name='image'
-                        placeholder='Insira uma imagem que representa o seu post'
+                        name="image"
                         required
-                        onChange={e => setImage(e.target.value)}
+                        placeholder="Insira uma imagem que representa seu post"
+                        onChange={(e) => setImage(e.target.value)}
                         value={image}
                     />
                 </label>
                 <label>
-                    <span>URL da imagem:</span>
+                    <span>Conteúdo:</span>
                     <textarea
-                        name='body'
-                        placeholder='Insira o conteúdo do post'
+                        name="body"
                         required
-                        onChange={e => setBody(e.target.value)}
+                        placeholder="Insira o conteúdo do post"
+                        onChange={(e) => setBody(e.target.value)}
                         value={body}
                     ></textarea>
                 </label>
@@ -61,17 +104,25 @@ const CreatePost = () => {
                     <span>Tags:</span>
                     <input
                         type="text"
-                        name='tags'
-                        placeholder='Insira as tags separadas por vírgula'
+                        name="tags"
                         required
-                        onChange={e => setTags(e.target.value)}
+                        placeholder="Insira as tags separadas por vírgula"
+                        onChange={(e) => setTags(e.target.value)}
                         value={tags}
                     />
                 </label>
-                <button className='btn'>Cadastrar</button>
+                {!response.loading && <button className="btn">Criar post!</button>}
+                {response.loading && (
+                    <button className="btn" disabled>
+                        Aguarde.. .
+                    </button>
+                )}
+                {(response.error || formError) && (
+                    <p className="error">{response.error || formError}</p>
+                )}
             </form>
         </div>
-    )
-}
+    );
+};
 
-export default CreatePost
+export default CreatePost;
