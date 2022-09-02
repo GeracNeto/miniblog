625a2378451724b7513b4bf11e7757058cefa9c6
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2022-09-02 17:58:50 -0300

    Estruturada dashboard

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -16,7 +16,7 @@ const Dashboard = () => {
     const uid = user.uid
 
     // Posts do usuário
-    const posts = []
+    const { documents: posts, loading } = useFetchDocuments("posts", null, uid)
 
     return (
         <div>
@@ -27,11 +27,15 @@ const Dashboard = () => {
                     <p>Não foram encontrados posts</p>
                     <Link to='/posts/create' className='btn'>Criar primeiro post</Link>
                 </div>
-            ): (
+            ) : (
                 <div>
                     <p>Tem posts!</p>
                 </div>
             )}
+
+            {posts && posts.map(post => (
+                <h3>{post.title}</h3>
+            ))}
         </div>
     )
 }
