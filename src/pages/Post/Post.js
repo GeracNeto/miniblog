08313eaaaca2669e2a9759e05188fb4ec09907d1
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2022-09-02 16:00:14 -0300

    Criada exibição individual dos posts

--- src/pages/Post/Post.js
+++ src/pages/Post/Post.js
@@ -2,14 +2,25 @@
 import styles from './Post.module.css'
 
 // Hooks
+import { useFetchDocument } from '../../hooks/useFetchDocument'
+
+// Libs
 import { useParams } from 'react-router-dom'
 
 const Post = () => {
 
     const { id } = useParams()
+    const { document: post, loading } = useFetchDocument("posts", id)
 
     return (
-        <div>Post {id}</div>
+        <div>
+            {loading && <p>Carregando post...</p>}
+            {post && (
+                <>
+                    <h1>{post.title}</h1>
+                </>
+            )}
+        </div>
     )
 }
 
