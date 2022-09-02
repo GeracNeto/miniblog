f90f51e090b390fa72a2479dbe07c5715b1fa5e0
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2022-09-02 17:43:31 -0300

    Atualizado Dashboard

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,9 +1,38 @@
 // CSS
 import styles from './Dashboard.module.css'
 
+// Libs
+import { Link } from 'react-router-dom'
+
+// Context
+import { useAuthValue } from '../../context/AuthContext'
+
+// Hooks
+import { useFetchDocuments } from '../../hooks/useFetchDocuments'
+
 const Dashboard = () => {
+
+    const { user } = useAuthValue()
+    const uid = user.uid
+
+    // Posts do usuário
+    const posts = []
+
     return (
-        <div>Dashboard</div>
+        <div>
+            <h2>Dashboard</h2>
+            <p>Gerencie seus posts</p>
+            {posts && posts.length === 0 ? (
+                <div className={styles.noposts}>
+                    <p>Não foram encontrados posts</p>
+                    <Link to='/posts/create' className='btn'>Criar primeiro post</Link>
+                </div>
+            ): (
+                <div>
+                    <p>Tem posts!</p>
+                </div>
+            )}
+        </div>
     )
 }
 
